34c8461e73eea9075b73deec2726f30f95fe91bb
gmanriqe <gmanriqe@gmail.com>
2022-09-30 08:22:25 -0500

    Mejora estructura de carpetas, uso de layout y mejora de uso de estilos

--- frontend-demo/components/Navbar.js
+++ frontend-demo/components/Navbar.js
@@ -1,12 +1,13 @@
-import Link from 'next/link'
+
 import styles from './Navbar.module.css'
+import { ActiveLink } from './ActiveLink'
 
 export const Navbar = () => {
   return (
     <nav className={ styles['menu-nav']}>
-        <Link href="">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+        <ActiveLink text="Home" href="/"/>
+        <ActiveLink text="About" href="/about"/>
+        <ActiveLink text="Contact" href="/contact"/>
     </nav>
   )
 }
--- frontend-demo/pages/about/index.js
+++ frontend-demo/pages/about/index.js
@@ -1,20 +1,19 @@
 import Link from 'next/link'
+import { MainLayout } from '../../components/layouts/MainLayout'
 
-import { MainLayout } from '../components/layouts/MainLayout'
-
-export default function Home() {
+export default function About() {
     return (
         <MainLayout>
-            <h2>Home Page</h2>
+            <h2>About Page</h2>
             <h1 className={'title'}>
                 {/* Ir a <a href="/about">About</a> */}
-                Ir a <Link href="/about">About</Link>
+                Ir a <Link href="/">Home</Link>
             </h1>
 
             <p className={'description'}>
                 Get started by editing{' '}
-                <code className={'code'}>pages/index.js</code>
+                <code className={'code'}>about/index.js</code>
             </p>
-        </MainLayout >
+        </MainLayout>
     )
 }
