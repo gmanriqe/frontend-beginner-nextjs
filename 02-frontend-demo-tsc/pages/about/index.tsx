9d78c7f04cec370ac93544ba1884dc6641be8a1e
gmanriqe <gmanriqe@gmail.com>
2022-10-01 18:24:07 -0500

    Fin 2da seccion

---
+++ 02-frontend-demo-tsc/pages/about/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { DarkLayout } from '../../components/layouts/DarkLayout'
+import { MainLayout } from '../../components/layouts/MainLayout'
+
+export default function About() {
+    return (
+        <>
+            <h2>About Page</h2>
+            <h1 className={'title'}>
+                {/* Ir a <a href="/about">About</a> */}
+                Ir a <Link href="/">Home</Link>
+            </h1>
+
+            <p className={'description'}>
+                Get started by editing{' '}
+                <code className={'code'}>about/about/index.js</code>
+            </p>
+        </>
+    )
+}
+
+About.getLayout = function getLayout(page: JSX.Element) {
+    return (
+        <MainLayout>
+            <DarkLayout>
+                {page}
+            </DarkLayout>
+        </MainLayout>
+    )
+}
